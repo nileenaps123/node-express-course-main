ac9516547d92185fd49f555760d6d9c6b1234b6b
Nileena <nileena.ps123@gmail.com>
2026-09-02 23:28:05 +0530

    task manager continuation

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,15 +3,15 @@ const express=require('express');
 const app=express();
 const tasks=require('./routes/tasks');
 const connectDB=require('./db/connect');
+const notFound=require('./middleware/not-found')
+const errHandler=require('./middleware/error-handler');
 require('dotenv').config();
+app.use(express.static('./public'))
 app.use(express.json());
-//routes
-app.get('/hello',(req,res)=>{
-    res.send('Task Manager App')
-});
-
 app.use('/api/v1/tasks',tasks)
-const port=3000
+app.use(notFound);
+app.use(errHandler);
+const port= process.env.PORT|| 3000
 const start=async()=>{
     try{
         await connectDB(process.env.MONGO_URL);
